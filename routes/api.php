0001b3237c81b3598b3289541b91136cbe9cdccd
uni51 <uni51@users.noreply.github.com>
2019-07-07 14:25:09 +0900

    finish ch04 認証API

---
+++ routes/api.php
@@ -0,0 +1,10 @@
+<?php
+
+// 会員登録
+Route::post('/register', 'Auth\RegisterController@register')->name('register');
+
+// ログイン
+Route::post('/login', 'Auth\LoginController@login')->name('login');
+
+// ログアウト
+Route::post('/logout', 'Auth\LoginController@logout')->name('logout');
---
+++ routes/web.php
@@ -0,0 +1,7 @@
+<?php
+
+// APIのURL以外のリクエストに対してはindexテンプレートを返す
+// 画面遷移はフロントエンドのVueRouterが制御する
+Route::get('/{any?}', function () {
+    return view('index');
+})->where('any', '.+');
