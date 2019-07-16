4a4b5a6f5f5c1ccfde572d1ff3ef215e9e70f79e
uni51 <uni51@users.noreply.github.com>
2019-07-16 12:56:00 +0900

    コメント投稿機能 フロントエンド

--- resources/js/pages/PhotoDetail.vue
+++ resources/js/pages/PhotoDetail.vue
@@ -25,12 +25,38 @@
       <h2 class="photo-detail__title">
         <i class="icon ion-md-chatboxes"></i>Comments
       </h2>
+      <ul v-if="photo.comments.length > 0" class="photo-detail__comments">
+        <li
+          v-for="comment in photo.comments"
+          v-bind:key="comment.content"
+          class="photo-detail__commentItem"
+        >
+          <p class="photo-detail__commentBody">
+            {{ comment.content }}
+          </p>
+          <p class="photo-detail__commentInfo">
+            {{ comment.author.name }}
+          </p>
+        </li>
+      </ul>
+      <p v-else>No comments yet.</p>      
+      <form v-if="isLogin" @submit.prevent="addComment" class="form">
+        <div v-if="commentErrors" class="errors">
+          <ul v-if="commentErrors.content">
+            <li v-for="msg in commentErrors.content" :key="msg">{{ msg }}</li>
+          </ul>
+        </div>        
+        <textarea class="form__item" v-model="commentContent"></textarea>
+        <div class="form__button">
+          <button type="submit" class="button button--inverse">submit comment</button>
+        </div>
+      </form>
     </div>
   </div>
 </template>
 
 <script>
-import { OK } from '../util'
+import { OK, CREATED, UNPROCESSABLE_ENTITY } from '../util'
 
 export default {
   props: {
@@ -42,9 +68,16 @@ export default {
   data () {
     return {
       photo: null,
-      fullWidth: false      
+      fullWidth: false,
+      commentContent: '',
+      commentErrors: null                  
     }
   },
+  computed: {
+    isLogin () {
+      return this.$store.getters['auth/check']
+    }
+  },  
   methods: {
     async fetchPhoto () {
       const response = await axios.get(`/api/photos/${this.id}`)
@@ -55,7 +88,34 @@ export default {
       }
 
       this.photo = response.data
-    }
+    },
+    async addComment () {
+      const response = await axios.post(`/api/photos/${this.id}/comments`, {
+        content: this.commentContent
+      })
+
+      // バリデーションエラー
+      if (response.status === UNPROCESSABLE_ENTITY) {
+        this.commentErrors = response.data.errors
+        return false
+      }      
+
+      this.commentContent = ''
+      // エラーメッセージをクリア
+      this.commentErrors = null
+
+      // その他のエラー
+      if (response.status !== CREATED) {
+        this.$store.commit('error/setCode', response.status)
+        return false
+      }
+      
+      // コメントの更新には $set メソッドを使う
+      this.$set(this.photo, 'comments', [
+        response.data,
+        ...this.photo.comments
+      ])      
+    }    
   },
   // $route を監視してページが切り替わったときに fetchPhoto が実行される  
   watch: {
